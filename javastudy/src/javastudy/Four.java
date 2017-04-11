92884435918b66677d354659266af72025e16fbc
shosepig <shosepig@naver.com>
2017-04-11 13:44:22 +0900

    .

---
+++ javastudy/src/javastudy/Four.java
@@ -0,0 +1,17 @@
+package javastudy;
+
+public class Four {
+
+	public static void main(String[] args) {
+		
+		int a = (int)(Math.random()*9+1);
+		int b = (int)(Math.random()*9+1);
+		System.out.println("A = " + a);
+		System.out.println("B = " + b);
+		System.out.println("A + B = " + (a + b));
+		System.out.println("A * B = " + (a * b));
+		System.out.println("A - B = " + (a - b));
+		System.out.println("A / B = " + (a / b));
+	}
+
+}
